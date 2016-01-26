b3b1fa12a7cc9cf452c36100ae3f456a40a0c0ca
marguerite <i@marguerite.su>
2016-01-26 20:43:22 +0800

    add nodejs(engine) Requires and finish random-picker

--- repo_maintenance/random-pick.rb
+++ repo_maintenance/random-pick.rb
@@ -1,6 +1,37 @@
 #!/usr/bin/env ruby
 
-# it will randomly pick n packages from devel:languages:nodejs
+# it will randomly pick <N> packages from <repo>
 # to branched repository like home:MargueriteSu:branches:devel:languages:nodejs
 # to test nodejs-packaging
 
+# ARGV[0]: repository name
+# ARGV[1]: number of N
+
+repo = ARGV[0] || "devel:languages:nodejs"
+if ARGV[0]
+	number = ARGV[1] || 10
+else
+	number = ARGV[0] || 10
+end
+
+# get packages
+
+list = []
+
+io = IO.popen("osc list #{repo}")
+io.each_line {|i| list << i.strip!}
+io.close
+
+list.reject! {|i| ! i.include?("nodejs")}
+
+# branched 
+
+size = list.size
+
+number.times do
+
+	io = IO.popen("osc branch #{repo} #{list[rand(size.to_i)]}")
+	io.each_line {|i| puts i}
+	io.close
+
+end
