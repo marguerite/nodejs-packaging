ad87b74a7d4ac5e26320b0e6812a0079873e601f
marguerite <i@marguerite.su>
2016-01-31 21:51:55 +0800

    save work for tommorow

--- tool/parent.rb
+++ tool/parent.rb
@@ -7,18 +7,23 @@ class Parent
 	end
 
 	def find(json=@json, parent=@parent)
-
+            
 	    unless json == nil
 	        unless json.key?(parent)
 			if json.keys.size == 1
 				@keys << json.keys[0]
 				self.find(json.values[0]["dependencies"],parent)
 			else
+				# FIXME: if both keys have the parent
 				json.keys.each do |k|
 					unless json[k]["dependencies"] == nil
 						if json[k]["dependencies"].to_s.index('"' + parent + '"')
-							@keys << k
-							self.find(json[k]["dependencies"],parent)
+							if @keys.include?(parent)
+								next
+							else
+								@keys << k
+								self.find(json[k]["dependencies"],parent)
+							end
 						end
 					end
 				end
@@ -53,4 +58,11 @@ class Parent
 	end
 
 end
+
+require 'json'
+str = ""
+File.open('test.json','r:UTF-8') {|f| str = f.read.gsub('=>',':')}
+json = JSON.parse(str)
+parent = "pinkie-promise"
+p Parent.new(json,parent).find
 
