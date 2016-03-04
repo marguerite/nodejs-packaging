31b430c6d5221e24896121805770b36eb3924380
marguerite <i@marguerite.su>
2016-03-04 22:07:46 +0800

    add --drop option to nodejs-fixdep.rb to drop dependency

--- nodejs-fixdep.rb
+++ nodejs-fixdep.rb
@@ -14,8 +14,13 @@ Dir.glob(buildroot + "/**/package.json") do |f|
 	files << f
 end
 
-pkgname = ARGV[0]
-dep = ARGV[1]
+if ARGV[0] == "--drop"
+  pkgname = ARGV[1]
+  dep = nil
+else
+  pkgname = ARGV[0]
+  dep = ARGV[1]
+end
 pkgjson = ""
 json = {}
 
@@ -25,7 +30,11 @@ files.each do |f|
         pkgjson = f.gsub('node_modules/' + pkgname + '/package.json','') + "package.json"
 	if File.exist? pkgjson
           File.open(pkgjson,'r:UTF-8') {|f| json = JSON.parse(f.read)}
-	  json["dependencies"][pkgname] = dep
+	  if ARGV[0] == "--drop"
+	    json["dependencies"].delete_if {|k,v| k == pkgname}
+	  else
+	    json["dependencies"][pkgname] = dep
+	  end
 	  File.open(pkgjson,'w:UTF-8') {|f| f.write JSON.pretty_generate(json) }
 	end
     end
