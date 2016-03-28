650c5a89d7fca55c5d652a5d9f6ca42686ab3020
marguerite <i@marguerite.su>
2016-03-28 17:45:24 +0800

    fix --drop option in nodejs-fixdep

--- nodejs-fixdep.rb
+++ nodejs-fixdep.rb
@@ -25,17 +25,24 @@ pkgjson = ""
 json = {}
 
 files.each do |f|
+  if ARGV[0] == "--drop"
+    # sometime the module needs to drop wasn't copyed to buildroot at all
+    File.open(f,'r:UTF-8') {|f| json = JSON.parse(f.read)}
+    unless json["dependencies"].nil?
+      unless json["dependencies"].keys.index(pkgname).nil?
+	json["dependencies"].delete_if {|i| i == pkgname}
+	File.open(f,'w:UTF-8') {|f1| f1.write JSON.pretty_generate(json) }
+      end
+    end
+  else
     if f.index(pkgname + "/package.json")
         # get its parent
         pkgjson = f.gsub('node_modules/' + pkgname + '/package.json','') + "package.json"
 	if File.exist? pkgjson
-          File.open(pkgjson,'r:UTF-8') {|f| json = JSON.parse(f.read)}
-	  if ARGV[0] == "--drop"
-	    json["dependencies"].delete_if {|k,v| k == pkgname}
-	  else
-	    json["dependencies"][pkgname] = dep
-	  end
-	  File.open(pkgjson,'w:UTF-8') {|f| f.write JSON.pretty_generate(json) }
+          File.open(pkgjson,'r:UTF-8') {|f1| json = JSON.parse(f1.read)}
+	  json["dependencies"][pkgname] = dep
+	  File.open(pkgjson,'w:UTF-8') {|f2| f2.write JSON.pretty_generate(json) }
 	end
     end
+  end
 end
