e11271c2fab95848739509851067f8dd1e6d8752
marguerite <i@marguerite.su>
2017-03-22 22:20:53 +0800

    refactor nodejs-check and nodejs-fixdep

--- nodejs-check.rb
+++ nodejs-check.rb
@@ -1,34 +1,37 @@
 #!/usr/bin/env ruby
+
 require 'json'
+require 'node2rpm'
 
-require '/usr/lib/rpm/nodejs/bundles.rb'
-include Bundles
-
-buildroot = Bundles.getbuildroot
-sitelib = Bundles.getsitelib
+$buildroot = Node2RPM::System.buildroot
+$sitelib = Node2RPM::System.sitelib
 
 # check where a module comes from
-mod = ARGV[0]
-
-unless mod.nil?
-  Dir.glob(buildroot + sitelib + "/**/package.json") do |f|
-    open(f,"r:UTF-8") do |file|
-      json = JSON.parse(file.read)
-      unless json["dependencies"].nil?
-        puts f if json["dependencies"].include?(mod)
-      end
-    end
+def check_module_origin
+  return if ARGV.empty?
+  Dir.glob($buildroot + $sitelib + '/**/package.json') do |file|
+    json = JSON.parse(open(file, 'r:UTF-8').read)
+    next if json['dependencies'].nil? || !json['dependencies'].include?(ARGV[0])
+    r = file.match(%r{^.*/(.*?)/package\.json})
+    puts r[1]
   end
 end
+
+check_module_origin
 
 # check left-over bower.json dependencies
-Dir.glob(buildroot + sitelib + "/**/*") do |f|
-  if f.end_with?("bower.json")
-    open(f,"r:UTF-8") do |file|
-      json = JSON.parse(file.read)
-      unless json["dependencies"].nil? || json["dependencies"].empty?
-	puts f.gsub(/^.*node_modules\//,'').gsub(/\/bower\.json/,'') + " has dependencies in bower.json, please do something"
-      end
+def left_bower_dependencies
+  files = Dir.glob($buildroot + $sitelib + '/**/bower_components/*')
+             .select { |x| File.directory?(x) }
+             .map! { |x| File.basename(x) }
+  Dir.glob($buildroot + $sitelib + '/**/*') do |file|
+    next unless file =~ %r{bower_components/.*/bower.json$}
+    json = JSON.parse(open(file, 'r:UTF-8').read)
+    next if json['dependencies'].nil?
+    json['dependencies'].keys.each do |k|
+      puts "#{file} has unmet bower dependency #{k}" unless files.include(k)
     end
   end
 end
+
+left_bower_dependencies
--- nodejs-fixdep.rb
+++ nodejs-fixdep.rb
@@ -1,48 +1,78 @@
 #!/usr/bin/env ruby
 
-require 'rubygems'
 require 'json'
-require '/usr/lib/rpm/nodejs/bundles.rb'
-include Bundles
+require 'node2rpm'
+require 'optparse'
+require 'ostruct'
 
-buildroot = Bundles.getbuildroot
+buildroot = Node2RPM::System.buildroot
+sitelib = Node2RPM::System.sitelib
+jsons = Dir.glob(buildroot + sitelib + '/**/package.json')
+options = OpenStruct.new
 
-## all directories
+parser = OptionParser.new do |opts|
+  opts.banner = 'Usage: nodejs-fixdep -m <mod> -b <range> -a <range> -d <dir>
+                 it will change the "module" with the "before" version
+                 to the "after" version, and replace the current dir
+                 in buildroot with provided one (can be skipped if not
+                 bundled packaging).
 
-files = []
-Dir.glob(buildroot + "/**/package.json") do |f|
-	files << f
-end
+                 nodejs-fixdep -m <mod> -a <range>
+                 it will change all the named modules to the "after"
+                 version.
 
-if ARGV[0] == "--drop"
-  pkgname = ARGV[1]
-  dep = nil
-else
-  pkgname = ARGV[0]
-  dep = ARGV[1]
-end
-pkgjson = ""
-json = {}
+                 nodejs-fixdep -m <mod> -b <range> -r
+                 it will remove all the named modules will that version.
 
-files.each do |f|
-  if ARGV[0] == "--drop"
-    # sometime the module needs to drop wasn't copyed to buildroot at all
-    File.open(f,'r:UTF-8') {|f| json = JSON.parse(f.read)}
-    unless json["dependencies"].nil?
-      unless json["dependencies"].keys.index(pkgname).nil?
-	json["dependencies"].delete_if {|i| i == pkgname}
-	File.open(f,'w:UTF-8') {|f1| f1.write JSON.pretty_generate(json) }
-      end
-    end
-  else
-    if f.index(pkgname + "/package.json")
-        # get its parent
-        pkgjson = f.gsub('node_modules/' + pkgname + '/package.json','') + "package.json"
-	if File.exist? pkgjson
-          File.open(pkgjson,'r:UTF-8') {|f1| json = JSON.parse(f1.read)}
-	  json["dependencies"][pkgname] = dep
-	  File.open(pkgjson,'w:UTF-8') {|f2| f2.write JSON.pretty_generate(json) }
-	end
-    end
+                 nodejs-fixdep -m <mod> -r
+                 it will completely remove the named modules everywhere.
+
+                 This macro should be used before %nodejs_filelist.'
+  opts.separator ''
+  opts.separator 'Specific Options:'
+
+  opts.on('-m <mod>', '--module <mod>', 'Specify the module to fix (Required)') do |mod|
+    options.mod = mod
   end
+
+  opts.on('-b <range>', '--before <range>', 'Specify the current version (semver supported)') do |range|
+    options.ver = range
+  end
+
+  opts.on('-a <range>', '--after <range>', 'Specify the fixed version (semver supported)') do |range|
+    options.fix = range
+  end
+
+  opts.on('-r', '--remove', 'Remove the module') do
+    options.removal = true
+  end
+
+  opts.on('-d <dir>', '--directory <dir>', 'Specify the directory to replace') do |dir|
+    options.dir = dir
+  end
+
+  opts.separator ''
+  opts.separator 'Common Options:'
+  opts.on_tail('-h', '--help', 'Show this message') do
+    puts opts
+    exit
+  end
+end
+
+parser.parse!(ARGV)
+
+jsons.each do |js|
+  json = JSON.parse(open(js, 'r:UTF-8').read)
+  next if json['dependencies'].nil? || json['dependencies'].empty? || !json['dependencies'].keys.include?(options.mod)
+  next if !options.ver.nil? && json['dependencies'][options.mod] != options.ver
+  dirs = Dir.glob(File.split(js)[0] + "/node_modules/#{options.mod}")
+  if options.removal
+    json['dependencies'].delete(options.mod)
+    FileUtils.rm_rf dirs[0] unless dirs.nil?
+  else
+    raise 'you should specify a fix' if options.fix.nil?
+    json['dependencies'][options.mod] = options.fix
+    FileUtils.rm_rf(dirs[0]) && FileUtils.mv(options.dir, dirs[0]) unless options.dir.nil? || dirs.nil?
+  end
+  open(js, 'w:UTF-8') { |f| f.write JSON.pretty_generate(json) }
 end
